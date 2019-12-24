de925f8ddf18b1fe15390d6164d5fbfc0c9e31bb
EricWehrly <eric@disruptiontheory.com>
2019-12-24 16:48:56 -0500

    hot reload

---
+++ wwwroot/js/blazor-hot-reload.js
@@ -0,0 +1,28 @@
+﻿
+var wait = ms => new Promise((r, j) => setTimeout(r, ms))
+var failed = false;
+
+async function fetchAsync() {
+    try {
+        let res = await fetch(window.location.href) // ping local server
+        //if failed before and now sucess then reload page
+        if (failed && res.status === 200) {
+            failed = false;
+            location.reload();
+        }
+    }
+    catch (e) {
+        failed = true;
+        await wait(200);
+        console.debug("failed" + e)
+    }
+}
+
+async function Pooling() {
+    while (true) {
+        fetchAsync();
+        await wait(200);
+    }
+}
+
+Pooling();
