6fb5b40a86f4b68015d38efedde6c2881c4f55e1
EricWehrly <eric@disruptiontheory.com>
2019-12-31 21:21:10 -0500

    clean up carousel appearance

--- GameObjects/Virus.cs
+++ GameObjects/Virus.cs
@@ -1,21 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BadActor.GameObjects
+﻿namespace BadActor.GameObjects
 {
     public class Virus : GameObject<Virus>
     {
+        public static double WriteTime(Exploit exploit, ViralVector vector)
+        {
+            // TODO: More complicated virus = longer scribe time
+            return vector.UnlockTime;
+        }
+
         public Exploit Exploit { get; private set; }
 
         public ViralVector Vector { get; private set; }
 
         public ViralDistributor DeliveryMechanism { get; private set; }
 
-        public Virus(string name)
+        public Virus(string name, Exploit exploit, ViralVector vector)
         {
             Name = name;
+
+            Exploit = exploit;
+
+            Vector = vector;
         }
     }
 }
